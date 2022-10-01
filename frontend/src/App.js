fdfdaff81434d4b91572066d2f51555bd5935a5e
Mason Wagoner <mgwagoner@gmail.com>
2022-10-01 13:52:47 -0400

    checking out to new branch"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 const App = () => {
   return (
     <div>
       <h1>Hello, World!</h1>
+      {/* <Button /> */}
     </div>
   );
 };
