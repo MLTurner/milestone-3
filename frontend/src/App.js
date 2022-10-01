d588cec0407f2184f224d9eed64595e1f7801877
Mason Wagoner <mgwagoner@gmail.com>
2022-10-01 12:47:00 -0400

    downloading dependencies in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { CssBaseline, Grid } from '@material-ui/core';
 
-import { Counter } from './features/counter/Counter';
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Map/Map.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Map = () => {
-    return(
-        <h1>Map</h1>
-    );
-}
-
-export default Map;
