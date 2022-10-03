9f37210a6f3fc00cc6c876fb14d5bed620620447
MLTurner <mlturne3@ncsu.edu>
2022-10-02 21:41:52 -0400

    building out Map component and adding Google API functionality

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import googleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab'
 import useStyles from './styles'
 
@@ -9,8 +9,26 @@ import useStyles from './styles'
 const Map = () => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)')
+
+    const coordinates = { lat: 0, lng: 0 };
+
     return(
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <googleMapReact 
+            /*API key string and data that should be secured will be converted to environment variables*/
+            bootstrapURLKeys={{ key: 'AIzaSyDMZB7D5md2w9B_zFb98ld6qtOL8C48p7g' }} 
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50, 50, 50, 50]}
+            options={''}
+            onChange={''}
+            
+            onChildClick={''}
+            >
+            </googleMapReact>
+
+        </div>
     );
 }
 
